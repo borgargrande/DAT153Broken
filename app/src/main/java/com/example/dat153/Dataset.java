3cfb6b26bba9c2fb5b1a590516d4f1ca0e210d63
577799 <577799@users.noreply.github.com>
2021-01-19 14:46:51 +0100

    activities layouts

--- app/src/main/java/com/example/dat153/Dataset.java
+++ app/src/main/java/com/example/dat153/Dataset.java
@@ -2,13 +2,28 @@ package com.example.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dataset extends AppCompatActivity {
+
+    Button buttonleggtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataset);
+
+        buttonleggtil = findViewById(R.id.buttonleggtil);
+
+        buttonleggtil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToLeggTilBilde = new Intent(Dataset.this, LeggTilBilde.class);
+                startActivity(goToLeggTilBilde);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dat153/MainActivity.java
+++ app/src/main/java/com/example/dat153/MainActivity.java
@@ -2,18 +2,42 @@ package com.example.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    Button buttonspill, buttondataset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate: ");
-    }
 
+        buttondataset = findViewById(R.id.buttonDataset);
+        buttonspill = findViewById(R.id.buttonSpill);
+
+        buttonspill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToSpill = new Intent(MainActivity.this, Spill.class);
+                startActivity(goToSpill);
+            }
+        });
+
+        buttondataset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToDataset = new Intent(MainActivity.this, Dataset.class);
+                startActivity(goToDataset);
+            }
+        });
+    }
 
 
 }
