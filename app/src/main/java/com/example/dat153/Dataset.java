a889c7c24bea84702eafdbdc657aba2b0b23ad52
577799 <577799@users.noreply.github.com>
2021-01-20 13:17:20 +0100

    added utils

--- app/src/main/java/com/example/dat153/Dataset.java
+++ app/src/main/java/com/example/dat153/Dataset.java
@@ -1,29 +1,70 @@
 package com.example.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.icu.util.Measure;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.dat153.Utils.GameObject;
+import com.example.dat153.Utils.MyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dataset extends AppCompatActivity {
 
     Button buttonleggtil;
+    RecyclerView recyclerview;
+    List<GameObject> allImages;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataset);
+//
+//        buttonleggtil = findViewById(R.id.buttonleggtil);
+//
+//        buttonleggtil.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent goToLeggTilBilde = new Intent(Dataset.this, LeggTilBilde.class);
+//                startActivity(goToLeggTilBilde);
+//            }
+//        });
 
-        buttonleggtil = findViewById(R.id.buttonleggtil);
+        GameObject one = new GameObject("Bilde 1", R.drawable.ic_testimage);
+        GameObject two = new GameObject("Bilde 2", R.drawable.ic_testimage);
+        GameObject three = new GameObject("Bilde 3", R.drawable.ic_testimage);
+        GameObject four = new GameObject("Bilde 4", R.drawable.ic_testimage);
+        GameObject five = new GameObject("Bilde 5", R.drawable.ic_testimage);
+        allImages = new ArrayList<>();
+        allImages.add(one);
+        allImages.add(two);
+        allImages.add(three);
+        allImages.add(four);
+        allImages.add(five);
 
-        buttonleggtil.setOnClickListener(new View.OnClickListener() {
+        recyclerview = findViewById(R.id.reclerview);
+        MyAdapter myadapter = new MyAdapter(Dataset.this, allImages);
+
+        myadapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent goToLeggTilBilde = new Intent(Dataset.this, LeggTilBilde.class);
-                startActivity(goToLeggTilBilde);
+            public void onDeleteClick(int position) {
+                allImages.remove(position);
+                myadapter.notifyItemRemoved(position);
             }
         });
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerview.setHasFixedSize(true);
+
+        recyclerview.setLayoutManager(layoutManager);
+        recyclerview.setAdapter(myadapter);
     }
 }
---
+++ app/src/main/java/com/example/dat153/Utils/GameObject.java
@@ -0,0 +1,27 @@
+package com.example.dat153.Utils;
+
+public class GameObject {
+    private String name;
+    private int image;
+
+    public GameObject(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
