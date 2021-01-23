be8a3475c7f005499dba849e7dfb938b22f9258d
577799 <577799@users.noreply.github.com>
2021-01-23 17:21:19 +0100

    my adapter fungerer i spill
    må lage ny "myadapter" for spillet

--- app/src/main/java/com/example/dat153/Spill.java
+++ app/src/main/java/com/example/dat153/Spill.java
@@ -1,6 +1,8 @@
 package com.example.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -11,6 +13,9 @@ import com.example.dat153.Utils.SharedObject;
 public class Spill extends AppCompatActivity {
 
     MyAdapter myadapter;
+    SharedObject sharedObject;
+    RecyclerView recyclerview;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,21 @@ public class Spill extends AppCompatActivity {
 
         myadapter = new MyAdapter(Spill.this, sharedObject.getAllObjectsShared());
         //myadapter.getItemCount();
+        myadapter.getItemId(1);
 
         ImageView imageView;
         imageView = (ImageView) findViewById(R.id.imageView1);
         //imageView =  findViewById(R.id.quizBilde);
         imageView.setImageResource(R.drawable.ic_testimage);
+
+
+        recyclerview = findViewById(R.id.recyclerview);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerview.setHasFixedSize(true);
+
+        recyclerview.setLayoutManager(layoutManager);
+        recyclerview.setAdapter(myadapter);
 
     }
 }
