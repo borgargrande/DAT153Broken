f8497bda3155e74cf484005636cdc6c946d71f81
577799 <577799@users.noreply.github.com>
2021-01-23 15:16:53 +0100

    startet på spill
    kan vise eksempel bilde på spill

--- app/src/main/java/com/example/dat153/Spill.java
+++ app/src/main/java/com/example/dat153/Spill.java
@@ -3,12 +3,29 @@ package com.example.dat153;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.dat153.Utils.MyAdapter;
+import com.example.dat153.Utils.SharedObject;
 
 public class Spill extends AppCompatActivity {
+
+    MyAdapter myadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spill);
+
+        SharedObject sharedObject = (SharedObject) getApplicationContext();
+
+        myadapter = new MyAdapter(Spill.this, sharedObject.getAllObjectsShared());
+        //myadapter.getItemCount();
+
+        ImageView imageView;
+        imageView = (ImageView) findViewById(R.id.imageView1);
+        //imageView =  findViewById(R.id.quizBilde);
+        imageView.setImageResource(R.drawable.ic_testimage);
+
     }
 }
