1d92d66d2a5bce7e79a820bb3cf294acdd55a06b
577799 <577799@users.noreply.github.com>
2021-01-28 14:55:39 +0100

    ferdig

--- app/src/main/java/com/example/dat153/Spill.java
+++ app/src/main/java/com/example/dat153/Spill.java
@@ -4,18 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dat153.Utils.MyAdapter;
 import com.example.dat153.Utils.SharedObject;
+import com.example.dat153.Utils.GameObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import  android.widget.Button;
+import android.widget.Toast;
+
 
 public class Spill extends AppCompatActivity {
 
-    MyAdapter myadapter;
     SharedObject sharedObject;
-    RecyclerView recyclerview;
-    RecyclerView.LayoutManager layoutManager;
+    ImageView imageView;
+    List<GameObject> allImages;
+    Button button;
+    EditText svarTekst;
+    TextView score, navn;
+    int min;
+    int max;
+    int korrekte;
+
+    Random random;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +43,65 @@ public class Spill extends AppCompatActivity {
         setContentView(R.layout.activity_spill);
 
         SharedObject sharedObject = (SharedObject) getApplicationContext();
-
-        myadapter = new MyAdapter(Spill.this, sharedObject.getAllObjectsShared());
-        //myadapter.getItemCount();
-        myadapter.getItemId(1);
-
-        ImageView imageView;
+        allImages = new ArrayList<>(sharedObject.getAllObjectsShared());
         imageView = (ImageView) findViewById(R.id.imageView1);
-        //imageView =  findViewById(R.id.quizBilde);
-        imageView.setImageResource(R.drawable.ic_testimage);
 
 
-        recyclerview = findViewById(R.id.recyclerview);
+        //vis et tilfeldig bilde
+        random = new Random();
+        min = 0;
+        max = allImages.size() -1 ;
+        //position = random.nextInt(max - min +1) +min;
+        position = random.nextInt(max - min+1)+min;
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerview.setHasFixedSize(true);
 
-        recyclerview.setLayoutManager(layoutManager);
-        recyclerview.setAdapter(myadapter);
+        imageView.setImageBitmap(allImages.get(position).getImage());
+
+
+        button = findViewById(R.id.answerButton);
+        svarTekst = findViewById(R.id.bildeNavn);
+        score = findViewById(R.id.textscore);
+
+
+
+        //vis/oppdater score
+        korrekte = 0;
+
+        //se om svar er korrekt
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(svarTekst.getText().toString().equals(allImages.get(position).getName())) {
+
+                    korrekte ++ ;
+                    score.setText("Din score er :" + korrekte);
+
+
+                    Toast.makeText(Spill.this, "korrekt", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(Spill.this, "feil", Toast.LENGTH_LONG).show();
+                }
+
+                if(allImages.size() > 1) {
+
+                    allImages.remove(position);
+
+                    max--;
+                    position = random.nextInt(max - min + 1) + min;
+
+                    imageView.setImageBitmap(allImages.get(position).getImage());
+                }else {
+                    //ferdig med bildene
+                    //vis kun score, fjern bilde. prøv igjen knapp.
+                }
+            }
+        });
+
+
+        //
+
+
+
 
     }
 }
